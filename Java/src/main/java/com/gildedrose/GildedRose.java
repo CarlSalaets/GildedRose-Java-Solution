f489573f7a497b53a7cbcc70bf6b0fbff21500e5
Carl Salaets <csalaets@isabel.eu>
2020-02-19 13:16:15 +0100

    refactor: extracted String constants into private static final fields

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,10 @@ package com.gildedrose;
 
 class GildedRose {
 
+	private static final String AGED_BRIE = "Aged Brie";
+	private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+	private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
 	Item[] items;
 
 	public GildedRose(Item[] items) {
@@ -15,10 +19,10 @@ class GildedRose {
 	}
 
 	private static void updateQuality(Item item) {
-		if (!item.name.equals("Aged Brie")
-				&& !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+		if (!item.name.equals(AGED_BRIE)
+				&& !item.name.equals(BACKSTAGE_PASSES)) {
 			if (item.quality > 0) {
-				if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+				if (!item.name.equals(SULFURAS)) {
 					item.quality--;
 				}
 			}
@@ -26,7 +30,7 @@ class GildedRose {
 			if (item.quality < 50) {
 				item.quality++;
 
-				if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+				if (item.name.equals(BACKSTAGE_PASSES)) {
 					if (item.sellIn < 11) {
 						if (item.quality < 50) {
 							item.quality++;
@@ -42,15 +46,15 @@ class GildedRose {
 			}
 		}
 
-		if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+		if (!item.name.equals(SULFURAS)) {
 			item.sellIn--;
 		}
 
 		if (item.sellIn < 0) {
-			if (!item.name.equals("Aged Brie")) {
-				if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+			if (!item.name.equals(AGED_BRIE)) {
+				if (!item.name.equals(BACKSTAGE_PASSES)) {
 					if (item.quality > 0) {
-						if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+						if (!item.name.equals(SULFURAS)) {
 							item.quality--;
 						}
 					}
